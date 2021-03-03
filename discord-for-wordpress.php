b392753eafc3cc4e4baf0f4f1d5a91f35656ca14
Ned_Tom <tdj800@gmail.com>
2021-03-03 17:17:51 +0100

    added base plugin script

---
+++ discord-for-wordpress.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Discord for Wordpress
+ * Plugin URI: #
+ * Description: Simple Discord widgets for Wordpress
+ * Version: 0.0.1
+ * Text Domain: dfwp
+ * Author: Ned_Tpm
+ * Author URI: https://craftblock.one
+*/
+
+
+
+// add styles
+// add javascript
+// > discord url loaders
+// > 
+// add shorcodes
+// > Loder functions
+// > 
+
+
+?>
